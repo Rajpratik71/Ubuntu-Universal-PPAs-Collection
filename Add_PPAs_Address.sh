6d2d2cc1f2d562273cbbc5069149c3f7ae7eb105
Pratik Raj <Rajpratik71@gmail.com>
2019-09-09 18:58:18 +0530

    making auto add feature

--- Add_PPAs_Address.sh
+++ Add_PPAs_Address.sh
@@ -1,92 +1,92 @@
-sudo add-apt-repository ppa:nilarimogard/webupd8
-sudo add-apt-repository ppa:pi-rho/security
-sudo add-apt-repository ppa:webupd8team/java
-sudo add-apt-repository ppa:x2go/stable
-sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:jd-team/jdownloader
-sudo add-apt-repository ppa:ubuntu-wine/ppa
-sudo add-apt-repository ppa:zentyal/2.3
-sudo add-apt-repository ppa:fbirlik/kscope
-sudo add-apt-repository ppa:ajf/trg
-sudo add-apt-repository ppa:caffeine-developers/caffeine-dev
-sudo add-apt-repository ppa:cardapio-team/cardapio-ppa
-sudo add-apt-repository ppa:colin-king/powermanagement
-sudo add-apt-repository ppa:jfi/ppa
-sudo add-apt-repository ppa:keithw/mosh
-sudo add-apt-repository ppa:keks9n/skypetab
-sudo add-apt-repository ppa:poliva/lightum-mba
-sudo add-apt-repository ppa:relan/exfat
-sudo add-apt-repository ppa:rye/ubuntuone-extras
-sudo add-apt-repository ppa:tualatrix/ppa
-sudo add-apt-repository ppa:xorg-edgers/ppa
-sudo add-apt-repository ppa:kde3-maintainers/trinity-svn-nightly
-sudo add-apt-repository ppa:kalon33/ppa
-sudo add-apt-repository ppa:nvidia-vdpau/ppa
-sudo add-apt-repository ppa:nvidia-vdpau/cutting-edge-multimedia
-sudo add-apt-repository ppa:alexmurray/indicator-sensors-daily
-sudo add-apt-repository ppa:broken/ppa
-sudo add-apt-repository ppa:caffeine-developers/caffeine-dev
-sudo add-apt-repository ppa:caffeine-developers/ppa
-sudo add-apt-repository ppa:dlech/keepass2-plugins
-sudo add-apt-repository ppa:dolik-rce/upp-nightly
-sudo add-apt-repository ppa:eugenesan/ppa
-sudo add-apt-repository ppa:faster3ck/converseen
-sudo add-apt-repository ppa:fbirlik/kscope
-sudo add-apt-repository ppa:fixnix/indicator-systemtray-unity
-sudo add-apt-repository ppa:freefilesync/ffs
-sudo add-apt-repository ppa:gencfsm/ppa
-sudo add-apt-repository ppa:git-core/ppa
-sudo add-apt-repository ppa:gnome3-team/gnome3-staging
-sudo add-apt-repository ppa:hsoft/ppa
-sudo add-apt-repository ppa:hunter-kaller/ppa
-sudo add-apt-repository ppa:indicator-multiload/daily
-sudo add-apt-repository ppa:indicator-multiload/stable-daily
-sudo add-apt-repository ppa:jd-team/jdownloader
-sudo add-apt-repository ppa:johnf-inodes/m3u8-segmenter
-sudo add-apt-repository ppa:jwigley/window-list
-sudo add-apt-repository ppa:keepassx/daily
-sudo add-apt-repository ppa:keithw/mosh
-sudo add-apt-repository ppa:kubuntu-ppa/experimental
-sudo add-apt-repository ppa:linrunner/tlp
-sudo add-apt-repository ppa:lubuntu-dev/lubuntu-daily
-sudo add-apt-repository ppa:mixxx/mixxx
-sudo add-apt-repository ppa:neon/kf5-snapshot-weekly
-sudo add-apt-repository ppa:nilarimogard/webupd8
-sudo add-apt-repository ppa:noobslab/indicators
-sudo add-apt-repository ppa:noobslab/themes
-sudo add-apt-repository ppa:paolorotolo/copy
-sudo add-apt-repository ppa:pinta-maintainers/pinta-stable
-sudo add-apt-repository ppa:pi-rho/security
-sudo add-apt-repository ppa:ravefinity-project/ppa
-sudo add-apt-repository ppa:razor-qt/ppa
-sudo add-apt-repository ppa:satyajit-happy/themes
-sudo add-apt-repository ppa:syncany/release
-sudo add-apt-repository ppa:teejee2008/ppa
-sudo add-apt-repository ppa:transmissionbt/ppa
-sudo add-apt-repository ppa:tuxpoldo/btsync
-sudo add-apt-repository ppa:ubuntu-mate-dev/ppa
-sudo add-apt-repository ppa:ubuntu-mate-dev/trusty-mate
-sudo add-apt-repository ppa:ubuntu-sdk-team/ppa
-sudo add-apt-repository ppa:ubuntu-wine/ppa
-sudo add-apt-repository ppa:unit3/bfgminer
-sudo add-apt-repository ppa:venerix/pkg
-sudo add-apt-repository ppa:videolan/stable-daily
-sudo add-apt-repository ppa:vovansrnd/coolreader
-sudo add-apt-repository ppa:webupd8team/gthumb
-sudo add-apt-repository ppa:webupd8team/java
-sudo add-apt-repository ppa:webupd8team/popcorntime
-sudo add-apt-repository ppa:webupd8team/themes
-sudo add-apt-repository ppa:webupd8team/tor-browser
-sudo add-apt-repository ppa:x2go/ppa
-sudo add-apt-repository ppa:x2go/stable
-sudo add-apt-repository ppa:yannubuntu/boot-repair
-sudo add-apt-repository ppa:ytvwld/syncthing
-sudo add-apt-repository "deb http://dl.google.com/linux/deb/ stable main non-free"
-sudo add-apt-repository "deb http://dl.google.com/linux/chrome/deb/ stable main"
-sudo add-apt-repository "deb http://dl.google.com/linux/talkplugin/deb/ stable main"
-sudo add-apt-repository "deb http://dl.google.com/linux/musicmanager/deb/ stable main"
-sudo add-apt-repository "deb http://dl.google.com/linux/earth/deb/ stable main"
-sudo add-apt-repository "deb http://dl.google.com/linux/chrome-remote-desktop/deb/ stable main"
-sudo add-apt-repository "deb http://download.virtualbox.org/virtualbox/debian precise contrib"
-sudo add-apt-repository "deb http://download.webmin.com/download/repository sarge contrib"
-sudo add-apt-repository "deb http://repo.ajenti.org/ng/debian main main ubuntu"
+sudo add-apt-repository --yes ppa:nilarimogard/webupd8
+sudo add-apt-repository --yes ppa:pi-rho/security
+sudo add-apt-repository --yes ppa:webupd8team/java
+sudo add-apt-repository --yes ppa:x2go/stable
+sudo add-apt-repository --yes ppa:git-core/ppa
+sudo add-apt-repository --yes ppa:jd-team/jdownloader
+sudo add-apt-repository --yes ppa:ubuntu-wine/ppa
+sudo add-apt-repository --yes ppa:zentyal/2.3
+sudo add-apt-repository --yes ppa:fbirlik/kscope
+sudo add-apt-repository --yes ppa:ajf/trg
+sudo add-apt-repository --yes ppa:caffeine-developers/caffeine-dev
+sudo add-apt-repository --yes ppa:cardapio-team/cardapio-ppa
+sudo add-apt-repository --yes ppa:colin-king/powermanagement
+sudo add-apt-repository --yes ppa:jfi/ppa
+sudo add-apt-repository --yes ppa:keithw/mosh
+sudo add-apt-repository --yes ppa:keks9n/skypetab
+sudo add-apt-repository --yes ppa:poliva/lightum-mba
+sudo add-apt-repository --yes ppa:relan/exfat
+sudo add-apt-repository --yes ppa:rye/ubuntuone-extras
+sudo add-apt-repository --yes ppa:tualatrix/ppa
+sudo add-apt-repository --yes ppa:xorg-edgers/ppa
+sudo add-apt-repository --yes ppa:kde3-maintainers/trinity-svn-nightly
+sudo add-apt-repository --yes ppa:kalon33/ppa
+sudo add-apt-repository --yes ppa:nvidia-vdpau/ppa
+sudo add-apt-repository --yes ppa:nvidia-vdpau/cutting-edge-multimedia
+sudo add-apt-repository --yes ppa:alexmurray/indicator-sensors-daily
+sudo add-apt-repository --yes ppa:broken/ppa
+sudo add-apt-repository --yes ppa:caffeine-developers/caffeine-dev
+sudo add-apt-repository --yes ppa:caffeine-developers/ppa
+sudo add-apt-repository --yes ppa:dlech/keepass2-plugins
+sudo add-apt-repository --yes ppa:dolik-rce/upp-nightly
+sudo add-apt-repository --yes ppa:eugenesan/ppa
+sudo add-apt-repository --yes ppa:faster3ck/converseen
+sudo add-apt-repository --yes ppa:fbirlik/kscope
+sudo add-apt-repository --yes ppa:fixnix/indicator-systemtray-unity
+sudo add-apt-repository --yes ppa:freefilesync/ffs
+sudo add-apt-repository --yes ppa:gencfsm/ppa
+sudo add-apt-repository --yes ppa:git-core/ppa
+sudo add-apt-repository --yes ppa:gnome3-team/gnome3-staging
+sudo add-apt-repository --yes ppa:hsoft/ppa
+sudo add-apt-repository --yes ppa:hunter-kaller/ppa
+sudo add-apt-repository --yes ppa:indicator-multiload/daily
+sudo add-apt-repository --yes ppa:indicator-multiload/stable-daily
+sudo add-apt-repository --yes ppa:jd-team/jdownloader
+sudo add-apt-repository --yes ppa:johnf-inodes/m3u8-segmenter
+sudo add-apt-repository --yes ppa:jwigley/window-list
+sudo add-apt-repository --yes ppa:keepassx/daily
+sudo add-apt-repository --yes ppa:keithw/mosh
+sudo add-apt-repository --yes ppa:kubuntu-ppa/experimental
+sudo add-apt-repository --yes ppa:linrunner/tlp
+sudo add-apt-repository --yes ppa:lubuntu-dev/lubuntu-daily
+sudo add-apt-repository --yes ppa:mixxx/mixxx
+sudo add-apt-repository --yes ppa:neon/kf5-snapshot-weekly
+sudo add-apt-repository --yes ppa:nilarimogard/webupd8
+sudo add-apt-repository --yes ppa:noobslab/indicators
+sudo add-apt-repository --yes ppa:noobslab/themes
+sudo add-apt-repository --yes ppa:paolorotolo/copy
+sudo add-apt-repository --yes ppa:pinta-maintainers/pinta-stable
+sudo add-apt-repository --yes ppa:pi-rho/security
+sudo add-apt-repository --yes ppa:ravefinity-project/ppa
+sudo add-apt-repository --yes ppa:razor-qt/ppa
+sudo add-apt-repository --yes ppa:satyajit-happy/themes
+sudo add-apt-repository --yes ppa:syncany/release
+sudo add-apt-repository --yes ppa:teejee2008/ppa
+sudo add-apt-repository --yes ppa:transmissionbt/ppa
+sudo add-apt-repository --yes ppa:tuxpoldo/btsync
+sudo add-apt-repository --yes ppa:ubuntu-mate-dev/ppa
+sudo add-apt-repository --yes ppa:ubuntu-mate-dev/trusty-mate
+sudo add-apt-repository --yes ppa:ubuntu-sdk-team/ppa
+sudo add-apt-repository --yes ppa:ubuntu-wine/ppa
+sudo add-apt-repository --yes ppa:unit3/bfgminer
+sudo add-apt-repository --yes ppa:venerix/pkg
+sudo add-apt-repository --yes ppa:videolan/stable-daily
+sudo add-apt-repository --yes ppa:vovansrnd/coolreader
+sudo add-apt-repository --yes ppa:webupd8team/gthumb
+sudo add-apt-repository --yes ppa:webupd8team/java
+sudo add-apt-repository --yes ppa:webupd8team/popcorntime
+sudo add-apt-repository --yes ppa:webupd8team/themes
+sudo add-apt-repository --yes ppa:webupd8team/tor-browser
+sudo add-apt-repository --yes ppa:x2go/ppa
+sudo add-apt-repository --yes ppa:x2go/stable
+sudo add-apt-repository --yes ppa:yannubuntu/boot-repair
+sudo add-apt-repository --yes ppa:ytvwld/syncthing
+sudo add-apt-repository --yes "deb http://dl.google.com/linux/deb/ stable main non-free"
+sudo add-apt-repository --yes "deb http://dl.google.com/linux/chrome/deb/ stable main"
+sudo add-apt-repository --yes "deb http://dl.google.com/linux/talkplugin/deb/ stable main"
+sudo add-apt-repository --yes "deb http://dl.google.com/linux/musicmanager/deb/ stable main"
+sudo add-apt-repository --yes "deb http://dl.google.com/linux/earth/deb/ stable main"
+sudo add-apt-repository --yes "deb http://dl.google.com/linux/chrome-remote-desktop/deb/ stable main"
+sudo add-apt-repository --yes "deb http://download.virtualbox.org/virtualbox/debian precise contrib"
+sudo add-apt-repository --yes "deb http://download.webmin.com/download/repository sarge contrib"
+sudo add-apt-repository --yes "deb http://repo.ajenti.org/ng/debian main main ubuntu"
